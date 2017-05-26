f1eaac51987a8ff0bcba025666ef776b5aa184e4
Abe Ge <yige@microsoft.com>
2017-05-26 19:41:08 +0800

    add bluebird to leverage promise in nodejs project. change webCrawler to promise chain.

--- Service/WebCrawler.ts
+++ Service/WebCrawler.ts
@@ -1,35 +1,47 @@
 /// <reference path="./../node_modules/@types/node/index.d.ts"/>
 /// <reference path="./../node_modules/@types/cheerio/index.d.ts"/>
+/// <reference path="./../node_modules/@types/bluebird/index.d.ts"/>
 
 import * as nConsole from "console";
 import * as http from "http";
 import * as cheerio from "cheerio";
+import * as b from "bluebird";
 
 module Abe.Service {
 
     export class WebCrawler {
-        public downloadPage() {
-            http.get("http://forms.office.com/", (res) => { this.getConent(res); });
+        public downloadPage(url: string) {
+            let defer = b.defer();
+            http.get(url,
+                (res) => {
+                    return this.getConent(res)
+                        .then(message => defer.resolve(message));
+                });
+            return defer.promise;
         }
 
         private getConent(response: http.IncomingMessage) {
+            let defer = b.defer();
             let message = "";
+
             response.on("data", (chunk) => {
                 message += chunk;
             });
             response.on("end", () => {
                 nConsole.log(message);
-                this.parseHTML(message);
+                defer.resolve(this.parseHTML(message));
             });
+            return defer.promise;
         }
 
         private parseHTML(content: string) {
             var _$ = cheerio.load(content);
             let contentList = _$("h1").toArray();
-            nConsole.log(contentList.map(value => _$(value).text()).join("\r\n"));
+            return contentList.map(value => _$(value).text()).join("\r\n");
         }
     }
 }
 
 var run = new Abe.Service.WebCrawler();
-run.downloadPage();
+run.downloadPage("http://www.test.com")
+    .then(content => nConsole.log(content));
