da8ad6423872ede64852030b2859f052db8b5678
Abe@Home <yige@microsoft.com>
2018-09-01 23:28:54 +0800

    update router and api

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import * as express from "express";
 import * as path from "path";
 import * as sampleRouter from "./router/sampleRouter";
+import * as bookRouter from "./router/bookRouter";
+import * as cacheRouter from "./router/cacheRouter";
 
 export class Demo {
     public static start() {
         let app = express();
         app.use(express.static(path.join(__dirname, "public")));
         app.use("/", sampleRouter);
+        app.use("/", bookRouter);
+        app.use("/", cacheRouter);
 
         let server = app.listen(3000, () => {
             console.log("[" + new Date().toUTCString() + "] Demo is listening port:%s", server.address().port);
--- src/router/bookRouter.ts
+++ src/router/bookRouter.ts
@@ -1,24 +1,30 @@
 
 import * as express from "express";
-import * as path from "path";
 import { BookService } from "../Service/bookService";
 import { HttpAgent } from "../Service/httpUtility";
 
 const routerName = "bookRouter";
 let router = express.Router();
 
-router.post('/book',(req,res)=>{
+router.post('/book', (req, res) => {
     let bookDomain = req.body.id;
     let bookId = req.body.bookId;
     let chapterId = req.body.chapterId;
     let bookService = new BookService(bookDomain, new HttpAgent());
+    bookService.getContent(bookId, chapterId)
+        .then(content => res.json(content));
+
     console.info(
         `${routerName}.book:
         Search book ${bookId} chapter ${chapterId}`);
 });
 
-router.post('/tableOfContent',(req,res)=>{
+router.post('/tableOfContent', (req, res) => {
     let bookId = req.query.id;
+    let bookService = new BookService("", new HttpAgent());
+    bookService.getTableOfContent(bookId)
+        .then(tableOfContent => res.json(tableOfContent));
+
     console.info(`${routerName}.tableOfContent:
     get list of book ${bookId}.`);
 });
