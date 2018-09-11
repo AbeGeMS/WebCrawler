f5ddc2b759cb69b5e4a2646f48c5ac2ec4f85f95
Abe Ge <yige@microsoft.com>
2018-09-11 21:09:51 +0800

    add book mark api

--- src/router/cacheRouter.ts
+++ src/router/cacheRouter.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import * as path from "path";
+import * as cookie from "cookie-parser";
 
 const routerName = "bookRouter";
 let router = express.Router();
@@ -11,4 +12,7 @@ router.get('/latestChapter',(req,res)=>{
 router.get('/tableOfContent',(req,res)=>{
 });
 
-export = router;
+router.get("/bookMark",(req,res)=>{
+});
+
+export = router
