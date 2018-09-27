1e7c7c08258b1fb82995ba5adb84c597d50cfac8
Abe Ge <yige@microsoft.com>
2018-09-27 21:12:59 +0800

    optimize mainpage code

--- src/public/script/view/mainPage.tsx
+++ src/public/script/view/mainPage.tsx
@@ -2,17 +2,4 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { HomePage } from "./homePage";
 
-interface IMainPageState {
-    test: any
-}
-
-export class MainPage extends React.Component<any, IMainPageState> {
-    public componentWillMount() {
-    }
-    public render() {
-        return <HomePage/> 
-    }
-}
-
-let mainPageProp = {};
-ReactDOM.render(<MainPage {...mainPageProp} />,document.getElementById("content-root"));
+ReactDOM.render(<HomePage/>,document.getElementById("content-root"));
