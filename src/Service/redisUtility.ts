ccfa6c47e329eb1381443b7ace5471fab6809584
Abe Ge <yige@microsoft.com>
2018-09-30 12:10:41 +0800

    remove useless code

--- src/Service/redisUtility.ts
+++ src/Service/redisUtility.ts
@@ -3,7 +3,7 @@ import { createClient } from "redis";
 
 const redisPort: number = 6380;
 const redisAddress: string = "myBookmark.redis.cache.windows.net";
-const redisPassword =""; 
+const redisPassword = "";
 
 export class RedisAgent {
     private _client = createClient(redisPort, redisAddress, {
@@ -15,23 +15,23 @@ export class RedisAgent {
 
     public SCAN(start: number, count: number): Promise<string[]> {
         return new Promise((resolve, reject) => {
-            try{
-            this._client.scan(start.toString(), 'COUNT', count.toString(), (err, reply) => {
-                if (!!err) {
-                    reject("scan resdis failed");
-                    console.log("WebCrawler.getBookList: scan redis Error is " + err);
-                    return;
-                }
+            try {
+                this._client.scan(start.toString(), 'COUNT', count.toString(), (err, reply) => {
+                    if (!!err) {
+                        reject("scan resdis failed");
+                        console.log("WebCrawler.getBookList: scan redis Error is " + err);
+                        return;
+                    }
 
-                if (reply.length != 2) {
-                    reject(`invalid book list in redis ${JSON.stringify(reply)}`);
-                    return;
-                };
-                resolve(reply[1]);
-            });
-        }catch(ex){
-            return reject(`RedisAgent.SCAN:Failed by ${ex}`);
-        }
+                    if (reply.length != 2) {
+                        reject(`invalid book list in redis ${JSON.stringify(reply)}`);
+                        return;
+                    };
+                    resolve(reply[1]);
+                });
+            } catch (ex) {
+                return reject(`RedisAgent.SCAN:Failed by ${ex}`);
+            }
         });
     }
 
--- src/Service/sampleService.ts
+++
@@ -1,5 +0,0 @@
-export class SampleService {
-    public SayHi(name: string): string {
-        return `Hello, my name is ${name}`;
-    }
-}
